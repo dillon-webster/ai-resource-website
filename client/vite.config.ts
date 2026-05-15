2589e96ede03ffdd319215ff50f0c62d92c84856
Dillon Webster <dillon.webster15@gmail.com>
2026-05-14 18:00:25 -0600

    chore: bust railpack build cache

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: { sourcemap: false },
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
